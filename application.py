fa38e681fe1c40cbf26fdee4f2c4ec9f034ea0e3
ninjasan <pmathur2@gmail.com>
2015-10-31 21:04:44 -0700

    feat: list_cities uses a template (and fake data)
    Created a template for the list_cities method.
    
    Adding a bunch of dictionaries to be used for data while working on the
    templates

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 __author__ = 'poojm'
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -11,13 +11,25 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
+#fake data for now
+city = {'name': "Seattle", 'id': 1}
+cities = [{'name': "Seattle", 'id': 1},
+          {'name': "Portland", 'id': 2},
+          {'name': "Vancouver", "id": 3},
+          {'name': "Chicago", "id": 4}]
+activity = {'name': "Greenlake", 'id': 1, 'city_id': 1, 'description': "walk around the lake", 'category': "outdoors"}
+activities = [{'name': "Greenlake", 'id': 1, 'city_id': 1, 'description': "walk around the lake", 'category': "outdoors"},
+              {'name': "mkt.", 'id': 2, 'city_id': 1, 'description': "eat delicious food by Ethan Stowell", 'category': "food"},
+              {'name': "Chihuly Glass Garden", 'id': 3, 'city_id': 1, 'description': "gorgeous glass museum", 'category': "museums"},
+              {'name': "Space Needle", 'id': 4, 'city_id': 1, 'description': "Visit Seattle's vision of the future from the 1969 World's Fair", 'category': "sightseeing"}]
 
 @app.route('/')
 @app.route('/home/')
 @app.route('/index/')
 @app.route('/cities/')
 def list_cities():
-    return "All the cities!"
+    return render_template('list_cities.html', cities=cities)
+    #return "All the cities!"
 
 
 @app.route('/cities/<int:city_id>/')
