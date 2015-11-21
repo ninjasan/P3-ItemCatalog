e36caa475f9aa58e8ff641934cd7f1c8fe7c7140
ninjasan <pmathur2@gmail.com>
2015-11-20 19:19:53 -0800

    style: updated code style
    app.py - all lines are 80 char or under
    - all functions are lower case with '_'
    - added doc strings for each function.

--- application.py
+++ application.py
@@ -38,10 +38,14 @@ def cities_JSON():
 @app.route('/cities/<int:city_id>/JSON/')
 def city_JSON(city_id):
     """
-    Returns a city in JSON format.
+    Provides:
+        Functionality to get a city and transform it into JSON
 
     Args:
-      city_id: the unique identifier for the city
+        city_id: the unique identifier for the city
+
+    Returns:
+        JSON formatted city information
     """
     city = get_city(city_id)
     return jsonify(city=city.serialize)
@@ -50,10 +54,14 @@ def city_JSON(city_id):
 @app.route('/cities/<int:city_id>/activities/JSON/')
 def city_activities_JSON(city_id):
     """
-    Returns list of activities for a city in JSON format.
+    Provides:
+        Functionality to get a activities for a city and transform it into JSON
 
     Args:
-      city_id: the unique identifier for the city.
+        city_id: the unique identifier for the city.
+
+    Returns:
+        JSON formatted list of activities for a city
     """
     activities = \
         session.query(Activity).filter(Activity.city_id == city_id).all()
@@ -62,11 +70,16 @@ def city_activities_JSON(city_id):
 
 @app.route('/cities/<int:city_id>/activities/<int:activity_id>/JSON/')
 def activity_JSON(city_id, activity_id):
-    """Returns an activity within a city in JSON format.
+    """
+    Provides:
+        Functionality to get an activity and transform it into JSON
 
     Args:
-      city_id: the unique identifier for the city.
-      activity_id: the unique identifier for the activity in that city
+        city_id: the unique identifier for the city.
+        activity_id: the unique identifier for the activity in that city
+
+    Returns:
+        JSON formatted activity information
     """
     activity = \
         session.query(Activity).filter(Activity.id == activity_id,
@@ -76,11 +89,13 @@ def activity_JSON(city_id, activity_id):
 
 @app.errorhandler(403)
 def action_unauthorized(error):
+    """Redirect the user to a site built 403 page"""
     return render_template('error_403.html', error=error), 403
 
 
 @app.errorhandler(404)
 def entity_not_found(error):
+    """Redirect the user to a site built 404 page"""
     return render_template('error_404.html', error=error), 404
 
 
@@ -90,7 +105,7 @@ def entity_not_found(error):
 @app.route('/login/')
 def login():
     """Gives this session a unique key and renders the login page."""
-    state = generate_state_key()
+    state = generate_key()
     login_session['state'] = state
     return render_template('login.html', state=state)
 
@@ -114,8 +129,8 @@ def list_cities():
 @app.route('/cities/new/', methods=['GET', 'POST'])
 def new_city():
     """
-    Provides functionality for adding a new city
-
+    Provides:
+        Functionality for adding a new city
         On GET: render the create city page
         On POST: create a new city and adds it to the DB
     """
@@ -140,13 +155,13 @@ def new_city():
 @app.route('/cities/<int:city_id>/edit/', methods=['GET', 'POST'])
 def edit_city(city_id):
     """
-    Provides functionality for editing a city
-
+    Provides:
+        Functionality for editing a city
         On GET: render the edit city page
         On POST: updates the city in the DB
 
     Args:
-      city_id: the unique identifier for the city.
+        city_id: the unique identifier for the city.
     """
     city = get_city(city_id)
     if request.method == 'POST':
@@ -206,10 +221,11 @@ def delete_city(city_id):
 @app.route('/cities/<int:city_id>/')
 def show_city(city_id):
     """
-    Provides functionality for displaying a city and associated activities
+    Provides:
+        Functionality for displaying a city and associated activities
 
     Args:
-      city_id: the unique identifier for the city.
+        city_id: the unique identifier for the city.
     """
     city = get_city(city_id)
     activities = session.query(Activity).filter(
@@ -258,14 +274,14 @@ def show_activity(city_id, activity_id):
 @app.route('/cities/<int:city_id>/activities/new/', methods=['GET', 'POST'])
 def new_activity(city_id):
     """
-    Provides functionality for adding a new activity for a city
-
+    Provides:
+        Functionality for adding a new activity for a city
         On GET: renders the form to add a new activity
         On POST: takes the input from the form and adds a new activity
                  to the DB
 
     Args:
-      city_id: the unique identifier for the city.
+        city_id: the unique identifier for the city.
     """
     city = get_city(city_id)
 
@@ -337,15 +353,15 @@ def edit_activity(city_id, activity_id):
            methods=['GET', 'POST'])
 def delete_activity(city_id, activity_id):
     """
-    Provides functionality to delete an activity in a city
-
+    Provides:
+        Functionality to delete an activity in a city
         On GET: renders the form to delete an activity
         On POST: takes the input from the form and deletes the activity
                  from the DB
 
     Args:
-      city_id: the unique identifier for the city.
-      activity_id: the unique identifier for the activity in that city
+        city_id: the unique identifier for the city.
+        activity_id: the unique identifier for the activity in that city
     """
     city = get_city(city_id)
     activity = get_activity(city_id, activity_id)
@@ -453,9 +469,9 @@ def gconnect():
     login_session['email'] = data["email"]
 
     # see if user exists
-    user_id = getUserId(data["email"])
+    user_id = get_user_id(data["email"])
     if not user_id:
-        user_id = createUser(login_session)
+        user_id = create_user(login_session)
     login_session['user_id'] = user_id
 
     output = ''
@@ -549,9 +565,9 @@ def fbconnect():
     login_session['picture'] = data["data"]["url"]
 
     # see if user exists
-    user_id = getUserId(login_session['email'])
+    user_id = get_user_id(login_session['email'])
     if not user_id:
-        user_id = createUser(login_session)
+        user_id = create_user(login_session)
     login_session['user_id'] = user_id
 
     output = ''
@@ -601,12 +617,13 @@ def disconnect():
 
 
 # helper functions
-def createUser(login_session):
+def create_user(login_session):
     """
-    Provides functionality to create a new user in the database
+    Provides:
+        Functionality to create a new user in the database
 
     Args:
-      login_session: the dictionary storing login details
+        login_session: the dictionary storing login details
     """
     newUser = User(name=login_session['username'],
                    email=login_session['email'],
@@ -618,29 +635,32 @@ def createUser(login_session):
     ).one()
     return user.id
 
-def getUserInfo(user_id):
+def get_user_info(user_id):
     """
-    Provides functionality retrieve the user's details from the database
+    Provides:
+        Functionality retrieve the user's details from the database
 
     Args:
-      user_id: the unique identifier for the user
+        user_id: the unique identifier for the user
 
     Returns:
-      user: an object containing the user's information
+        user: an object containing the user's information
     """
     user = session.query(User).filter(User.id == user_id).one()
     return user
 
-def getUserId(email):
+def get_user_id(email):
     """
-    Provides functionality to see if a user has already been registered
+    Provides:
+        Functionality to see if a user has already been registered
 
     Args:
-      email: the email address of the user, from their Facebook or Google account
+        email: the email address of the user, from their Facebook
+            or Google account
 
     Returns:
-      user.id: the unique identifier for the user, if the user has registered
-      none: if the user has not registered yet
+        user.id or none: the unique identifier for the user,
+            if the user has registered
     """
     try:
         user = session.query(User).filter(User.email == email).one()
@@ -650,6 +670,17 @@ def getUserId(email):
 
 
 def get_city(city_id):
+    """
+    Provides:
+        functionality to get the city from the DB, if it exists
+        If the city doesn't exist, we abort with a 404
+
+    Args:
+        city_id: the identifier for the city we're trying to find
+
+    Returns:
+        an object representing the city
+    """
     try:
         city = session.query(City).filter(City.id == city_id).one()
     except NoResultFound, error:
@@ -658,34 +689,73 @@ def get_city(city_id):
 
 
 def get_activity(city_id, activity_id):
+    """
+    Provides:
+        Functionality to get the activity from the DB, if it exists
+        If the activity doesn't exist, we abort with a 404
+
+    Args:
+        city_id: the identifier for the city in which the activity exists
+        activity_id: the identifier for the activity we're trying to find
+
+    Returns:
+        an object representing the activity
+    """
     try:
-        activity = session.query(Activity).filter(Activity.id == activity_id,
-                                                  Activity.city_id == city_id).one()
+        activity = session.query(Activity).filter(
+            Activity.id == activity_id,
+            Activity.city_id == city_id).one()
     except NoResultFound, error:
         abort(404, error)
     return activity
 
 
-def get_creator(item):
+def get_creator(creator_id):
+    """
+    Provides:
+        Functionality to get the creator from the DB, if he/she exists
+        If the creator doesn't exist, we abort with a 404
+
+    Args:
+        creator_id: the identifier for the user who created the item
+
+    Returns:
+        an object representing the creator of the item
+    """
     try:
-        creator = session.query(User).filter(User.id == item.user_id).one()
+        creator = session.query(User).filter(User.id == creator_id).one()
     except NoResultFound, error:
         abort(404, error)
     return creator
 
 
-def generate_state_key():
+def generate_key():
     """
-    Provides functionality to create a random one-time use key
+    Provides:
+        Functionality to create a random code
 
     Returns:
-      a random one-time use key
+        a random string of characters
+    """
+    return ''.join(
+            random.choice(string.ascii_uppercase + string.digits)
+            for x in xrange(32))
+
+
+def generate_nonce():
+    """
+    Provides:
+        Functionality to create a random one-time use key
+
+    Returns:
+        a random one-time use key
     """
     if 'nonce' not in login_session:
-        login_session['nonce'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+        login_session['nonce'] = generate_key()
     return login_session['nonce']
-app.jinja_env.globals['nonce'] = generate_state_key
-    
+app.jinja_env.globals['nonce'] = generate_nonce
+
+
 if __name__ == '__main__':
     app.secret_key = "super_secret_key"
     app.debug = True
