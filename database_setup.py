d9fa44314cd1e86b6c04985609b7e2922ba3c2bb
ninjasan <pmathur2@gmail.com>
2015-11-21 16:45:22 -0800

    style: adding comments to db setup file

--- database_setup.py
+++ database_setup.py
@@ -7,6 +7,19 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class User(Base):
+    """Summary of User Class.
+
+    Object that contains information about registered users
+
+    Attributes:
+        id: An integer representing a unique identifier for the user
+        name: A string representing the name of the user, as it was specified
+                on the 3rd party, that was used to sign-in for the first time
+        email: A string representing the email address for the user, from the
+                3rd party site that was used to sign-in, for the first time
+        picture_url: A string representing the picture for the user, from the
+                3rd party site that was used to sign-in, for the first time
+    """
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(100))
@@ -15,6 +28,21 @@ class User(Base):
 
 
 class City(Base):
+    """Summary of City Class.
+
+    Object that contains information about a City
+
+    Attributes:
+        id: An integer representing a unique identifier for the city
+        user_id: An integer representing the id of the user who create it
+        name: A string representing the name of the city
+        state_provence: A string representing the state or provence in which
+                this city exists
+        country: A string representing the country in which this city exists
+        description: A string written by the user that can be used to learn
+                more about a location
+        image: A string representing the url of an image for the city
+    """
     __tablename__ = 'city'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
@@ -27,19 +55,37 @@ class City(Base):
 
     @property
     def serialize(self):
-        #Returns object in easily seriabilizable format
+        """Returns object in easily seriabilizable format"""
         return {
-            "name": self.name,
-            "id": self.id,
-            "user_id": self.user_id,
-            "state_provence": self.state_provence,
-            "country": self.country,
-            "description": self.description,
-            "image": self.image
+            'name': self.name,
+            'id': self.id,
+            'user_id': self.user_id,
+            'state_provence': self.state_provence,
+            'country': self.country,
+            'description': self.description,
+            'image': self.image
         }
 
 
 class Activity(Base):
+    """Summary of Activity Class.
+
+    Object that contains information about a City
+
+    Attributes:
+        id: An integer representing a unique identifier for the activity
+        city_id: An integer representing a unique identifier for the city
+                that has this activity
+        user_id: An integer representing the id of the user who create it
+        name: A string representing the name of the activity
+        category: A string representing the category that the activity
+                falls in
+        description: A string written by the user that can be used to learn
+                more about the activity
+        website: A string representing the url of the website for an activity
+        address: A string representing the street address of the activity
+        image: A string representing the url of an image for the activity
+    """
     __tablename__ = 'activity'
     name = Column(String(100), nullable=False)
     id = Column(Integer, primary_key=True)
@@ -55,17 +101,17 @@ class Activity(Base):
 
     @property
     def serialize(self):
-        #Returns object in easily seriabilizable format
+        """Returns object in easily seriabilizable format"""
         return {
-            "name": self.name,
-            "id": self.id,
-            "city_id": self.city_id,
-            "user_id": self.user_id,
-            "category": self.category,
-            "description": self.description,
-            "website": self.website,
-            "address": self.address,
-            "image": self.image
+            'name': self.name,
+            'id': self.id,
+            'city_id': self.city_id,
+            'user_id': self.user_id,
+            'category': self.category,
+            'description': self.description,
+            'website': self.website,
+            'address': self.address,
+            'image': self.image
         }
 
 
